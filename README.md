a8c21838bae38e3620bbcfaa57fc032adeb26ab4
baptistebaquero <83285614+baptistebaquero@users.noreply.github.com>
2022-04-04 14:34:43 -0400

    removing pytorch3d of the requirement

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Contributors: Baptiste Baquero, Juan Prieto, Maxime Gillot, Lucia Cevidanes
 
 ## What is it?
-ALIDDM is an approach to capture 2D views from intra oral scan and use the generated images to train deep learning algorithms or make a prediction once you have a trained model. 
+ALIDDM is an approach to capture 2D views from intra oral scan and use the generated images to train deep learning algorithms or make a prediction when you have a trained model. 
 
 ## How it works?
 It scales the mesh to the unit sphere, it captures 2D views from 5 different viewpoints, and it's doing this for each tooth. For each camera the neural  network segment patches on the surface to finally recover the final position after an average of every coordinates point in the patch and an upsampling step to the original scale.
--- py/prediction.py
+++ py/prediction.py
@@ -212,7 +212,7 @@ if __name__ == '__main__':
     input_param.add_argument('--jaw',type=str,help="Prepare the data for uper or lower landmark training (ex: L U)", default="U")
     input_param.add_argument('--sphere_radius', type=float, help='Radius of the sphere with all the cameras', default=0.2)
     input_param.add_argument('--label_L', type=list, help='label of the teeth',default=["18","19","20","21","22","23","24","25","26","27","28","29","30","31"])
-    input_param.add_argument('--label_U', type=list, help='label of the teeth',default=(["2","3","4","5","6","7","8","9","10","11","12","13","14","15"]))
+    input_param.add_argument('--label_U', type=list, help='label of the teeth',default=["2","3","4","5","6","7","8","9","10","11","12","13","14","15"])
 
     # Prediction data
     input_param.add_argument('--image_size',type=int, help='size of the picture', default=224)
